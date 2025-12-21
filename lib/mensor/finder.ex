c97c68079f432ac59b107998f9e5ee307fe2d197
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-12-21 19:11:15 +0200

    Add reading the .sln file and extrating project paths form it.

--- lib/mensor/finder.ex
+++ lib/mensor/finder.ex
@@ -16,26 +16,48 @@ defmodule Mensor.Finder do
   def handle_continue(:init, nil) do
     IO.inspect(@start_path)
 
-    Mensor.Finder.FileEnumeration.enumerate(@start_path)
-    |> IO.inspect()
+    projFiles =
+      Mensor.Finder.FilepathEnumeration.proj_files(@start_path)
+      |> IO.inspect()
 
-    # TODO: find *proj files
-    # TODO: send their paths to discover dependencies
+    slnPaths =
+      Mensor.Finder.FilepathEnumeration.solution_proj_files(@start_path <> ".sln")
+      |> IO.inspect()
+
+    diff =
+      MapSet.symmetric_difference(MapSet.new(projFiles), MapSet.new(slnPaths))
+      |> IO.inspect()
 
     {:noreply, nil}
   end
 end
 
-defmodule Mensor.Finder.FileEnumeration do
-  def enumerate(path) do
+defmodule Mensor.Finder.FilepathEnumeration do
+  def enumerate_filepaths(path) do
     File.ls!(path)
     |> Enum.map(&PathHelpers.join(path, &1))
     |> Enum.map(fn p ->
       case File.dir?(p) do
         false -> p
-        true -> enumerate(p)
+        true -> enumerate_filepaths(p)
       end
     end)
     |> List.flatten()
+  end
+
+  def proj_files(path) do
+    enumerate_filepaths(path)
+    |> Enum.filter(fn x -> String.ends_with?(x, "proj") end)
+  end
+
+  def solution_proj_files(path) do
+    File.stream!(path)
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(&String.starts_with?(&1, "Project("))
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.filter(fn x -> String.ends_with?(Enum.at(x, 1), "proj\"") end)
+    |> Enum.map(&Enum.at(&1, 1))
+    |> Enum.map(fn x -> String.slice(x, 2..(String.length(x) - 2)) end)
+    |> Enum.map(fn x -> PathHelpers.parent(path) <> "\\" <> x end)
   end
 end
--- lib/path_helpers.ex
+++ lib/path_helpers.ex
@@ -8,4 +8,23 @@ defmodule PathHelpers do
   def join(left, right, separator) do
     left <> separator <> right
   end
+
+  def parent(path) do
+    sep_pos = separator_at(path)
+    String.slice(path, 0..(sep_pos - 1))
+  end
+
+  def separator_at(str) do
+    len = String.length(str) - 1
+    separator_at(str, len)
+  end
+
+  defp separator_at(str, pos) do
+    case String.at(str, pos) do
+      nil -> -1
+      "\\" -> pos
+      "/" -> pos
+      _ -> separator_at(str, pos - 1)
+    end
+  end
 end
