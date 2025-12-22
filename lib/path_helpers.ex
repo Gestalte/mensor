7f3629fd1c615c4926c521cf831f0cf5e3d06c21
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-12-22 02:05:40 +0200

    Add more helper functions to path_helpers.ex

--- lib/path_helpers.ex
+++ lib/path_helpers.ex
@@ -14,7 +14,38 @@ defmodule PathHelpers do
     String.slice(path, 0..(sep_pos - 1))
   end
 
-  def separator_at(str) do
+  def filename(path, include_extension \\ true) do
+    case include_extension do
+      true ->
+        len = String.length(path) - 1
+        IO.inspect(len)
+        sep_pos = find_char_position(path, "\\", len)
+        IO.inspect(sep_pos)
+        String.slice(path, (sep_pos + 1)..len)
+
+      false ->
+        len = String.length(path) - 1
+        sep_pos = find_char_position(path, "\\")
+        base = String.slice(path, (sep_pos + 1)..len)
+        period_pos = find_char_position(base, ".")
+        String.slice(base, 0..(period_pos - 1))
+    end
+  end
+
+  def find_char_position(str, char) do
+    len = String.length(str) - 1
+    find_char_position(str, char, len)
+  end
+
+  def find_char_position(str, char, pos) do
+    case String.at(str, pos) do
+      nil -> -1
+      ^char -> pos
+      _ -> find_char_position(str, char, pos - 1)
+    end
+  end
+
+  defp separator_at(str) do
     len = String.length(str) - 1
     separator_at(str, len)
   end
